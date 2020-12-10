279240e6f0451cef9b17ec4d0c61bbf60c821431
Ethan Kent <ekent@spoonflower.com>
2020-12-10 09:20:07 -0500

    MVP using METARs

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,243 @@
 use reqwest;
 use serde::{Deserialize, Serialize};
+use serde_xml_rs::from_str;
+use std::collections::HashMap;
 use std::error::Error;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Welcome {
-    properties: Properties,
+#[derive(Debug)]
+struct TempData {
+    temp_c: f64,
+    temp_f: f64,
+    wind_chill_f: f64,
+    heat_index_f: f64,
+}
+
+impl TempData {
+    pub fn new(metar: &Metar) -> Self {
+        let temp_c = metar.temp_c as f64;
+        let temp_f = celsius_to_fahrenheit(temp_c);
+        let rh = compute_relative_humidity(temp_c, metar.dewpoint_c as f64);
+        let heat_index_f = rh
+            .and_then(|rh| Ok(compute_heat_index(temp_c, rh)))
+            .unwrap_or(temp_f);
+
+        TempData {
+            temp_c,
+            temp_f,
+            heat_index_f,
+            wind_chill_f: compute_wind_chill(temp_f, metar.wind_speed_kt).unwrap_or(temp_f),
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Properties {
-    temperature: Temperature,
-    #[serde(rename = "apparentTemperature")]
-    apparent_temperature: Temperature,
+pub struct Metar {
+    dewpoint_c: f32,
+    observation_time: String,
+    station_id: String,
+    temp_c: f32,
+    wind_speed_kt: usize,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Temperature {
-    uom: String,
-    values: Vec<Value>,
+pub struct Data {
+    #[serde(rename = "METAR")]
+    metar: Vec<Metar>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Value {
-    #[serde(rename = "validTime")]
-    valid_time: String,
-    value: f64,
+pub struct Response {
+    pub data: Data,
 }
 
-const CHAPEL_HILL: &'static str = "https://api.weather.gov/gridpoints/RAH/56,61";
-const OAK_PARK: &'static str = "https://api.weather.gov/gridpoints/LOT/69,71";
-
-async fn get_temperature(client: &reqwest::Client, uri: &str) -> Result<f64, Box<dyn Error>> {
-    Ok(client
-        .get(uri)
-        .header(reqwest::header::USER_AGENT, "experimenting")
-        .send()
-        .await?
-        .json::<Welcome>()
-        .await?
-        .properties
-        .temperature
-        .values[0]
-        .value)
-}
-
-fn convert_to_fahrenheit(celsius: f64) -> f64 {
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
     celsius * 9.0 / 5.0 + 32.0
 }
+
+fn convert_knots_to_mph(knots: usize) -> f64 {
+    ((1.15078 * knots as f64) * 100.00).round() / 100.0
+}
+
+fn compute_wind_chill(fahrenheit: f64, knots: usize) -> Option<f64> {
+    let mph = convert_knots_to_mph(knots);
+    if mph >= 3.0 && fahrenheit < 50.0 {
+        Some(
+            35.74 + (0.6215 * fahrenheit) - (35.75 * mph.powf(0.16))
+                + (0.4275 * fahrenheit * mph.powf(0.16)),
+        )
+    } else {
+        None
+    }
+}
+
+fn compute_relative_humidity(celsius: f64, dewpoint: f64) -> Result<f64, ()> {
+    if dewpoint > celsius {
+        Err(())
+    } else {
+        Ok((10000.0 * ((17.625 * dewpoint) / (243.04 + dewpoint)).exp()
+            / ((17.625 * celsius) / (243.04 + celsius)).exp())
+        .round()
+            / 100.0)
+    }
+}
+
+fn compute_heat_index(celsius: f64, rh: f64) -> f64 {
+    if celsius < 27.0 || rh < 40.0 {
+        return celsius;
+    }
+
+    let c1 = -8.78469475556;
+    let c2 = 1.61139411;
+    let c3 = 2.33854883889;
+    let c4 = -0.14611605;
+    let c5 = -0.012308094;
+    let c6 = -0.0164248277778;
+    let c7 = 0.002211732;
+    let c8 = 0.00072546;
+    let c9 = -0.000003582;
+
+    c1 + c2 * celsius
+        + c3 * rh
+        + c4 * celsius * rh
+        + c5 * celsius * celsius
+        + c6 * rh * rh
+        + c7 * celsius * celsius * rh
+        + c8 * celsius * rh * rh
+        + c9 * celsius * celsius * rh * rh
+}
+
+fn group_by<'a, T, U>(to_group: &'a Vec<T>, group_fn: fn(&'a T) -> U) -> HashMap<U, Vec<&'a T>>
+where
+    U: std::cmp::Eq + std::hash::Hash,
+{
+    to_group.iter().fold(HashMap::new(), |mut acc, el| {
+        let to_insert = group_fn(el);
+        let entry = acc.entry(to_insert).or_insert(Vec::new());
+        entry.push(el);
+        acc
+    })
+}
+
+const CHICAGO: &'static str = "KMDW";
+const RALEIGH: &'static str = "KRDU";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let client = reqwest::Client::new();
-    let chapel_hill_temp = convert_to_fahrenheit(get_temperature(&client, &CHAPEL_HILL).await?);
-    let oak_park_temp = convert_to_fahrenheit(get_temperature(&client, &OAK_PARK).await?);
-    println!(
-        "It is {}º warmer in Chapel Hill",
-        chapel_hill_temp - oak_park_temp
+    let response = client
+        .get(r#"https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KMDW%20KRDU&hoursBeforeNow=1"#)
+        .header(reqwest::header::USER_AGENT, "experimenting")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let deserialized: Response = from_str(&response)?;
+    let grouped = group_by(&deserialized.data.metar, |el| &el.station_id);
+    let mdw = TempData::new(
+        grouped
+            .get(&CHICAGO.to_string())
+            .and_then(|arr| {
+                arr.iter()
+                    .max_by(|metar1, metar2| metar1.observation_time.cmp(&metar2.observation_time))
+            })
+            .ok_or("Chicago weather data not in expected format")?,
     );
+    let rdu = TempData::new(
+        grouped
+            .get(&RALEIGH.to_string())
+            .and_then(|arr| {
+                arr.iter()
+                    .max_by(|metar1, metar2| metar1.observation_time.cmp(&metar2.observation_time))
+            })
+            .ok_or("Raleigh weather data not in expected format")?,
+    );
+
+    let amount_colder_chicago = rdu.temp_f - mdw.temp_f;
+    let amount_wind_chill_colder_chicago = rdu.wind_chill_f - mdw.wind_chill_f;
+
+    println!(
+        "Right now it's {:.1}ºF {} in Oak Park",
+        amount_colder_chicago.abs(),
+        if amount_colder_chicago > 0.0 {
+            "colder"
+        } else {
+            "warmer"
+        }
+    );
+    println!(
+        "Right now the wind chill is {:.1}ºF {} in Oak Park",
+        amount_wind_chill_colder_chicago.abs(),
+        if amount_wind_chill_colder_chicago > 0.0 {
+            "colder"
+        } else {
+            "warmer"
+        }
+    );
+
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_celsius_to_fahrenheit() {
+        assert_eq!(celsius_to_fahrenheit(0.0), 32.0);
+        assert_eq!(celsius_to_fahrenheit(-40.0), -40.0);
+        assert_eq!(celsius_to_fahrenheit(100.0), 212.0);
+    }
+
+    #[test]
+    fn test_convert_knots_to_mph() {
+        assert_eq!(convert_knots_to_mph(25), 28.77);
+    }
+    #[test]
+    fn test_compute_wind_chill() {
+        assert_eq!(compute_wind_chill(20.0, 5).unwrap().round(), 12.0);
+
+        assert!(compute_wind_chill(50.0, 5).is_none());
+        assert!(compute_wind_chill(49.9, 5).is_some());
+
+        assert!(compute_wind_chill(40.0, 2).is_none());
+        assert!(compute_wind_chill(40.0, 3).is_some());
+    }
+
+    #[test]
+    fn test_compute_relative_humidity() {
+        assert_eq!(compute_relative_humidity(22.0, 18.0).unwrap(), 78.06);
+    }
+
+    fn within_heat_index_error(actual: f64, expected: f64) -> bool {
+        (actual - expected).abs() <= 0.7
+    }
+
+    #[test]
+    fn test_compute_heat_index() {
+        assert!(within_heat_index_error(
+            compute_heat_index(27.0, 40.0),
+            27.0
+        ));
+        assert!(within_heat_index_error(
+            compute_heat_index(31.0, 55.0),
+            34.0
+        ));
+        assert!(within_heat_index_error(
+            compute_heat_index(31.0, 85.0),
+            43.0
+        ));
+    }
+
+    #[test]
+    fn test_group_by() {
+        let mut expected = HashMap::new();
+        expected.insert(false, vec![&3, &7, &5, &19, &33]);
+        expected.insert(true, vec![&2, &14]);
+
+        assert_eq!(
+            group_by(&vec![3, 7, 2, 5, 19, 33, 14], |el| el % 2 == 0),
+            expected
+        );
+    }
 }
